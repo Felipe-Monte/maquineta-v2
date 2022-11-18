38b8e8852cec7687b432d3d2241612ad6c5cc3f7
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-11-18 19:26:35 -0300

    update maquineta

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,7 +9,6 @@ function myScope() {
         let num = document.getElementById('txtn1')
         let numUser = Number(num.value)
         res.innerHTML = ''
-        num.value = ''
         num.focus()
 
         if (numUser > 50000 || numUser === 0 || numUser < 1) {
@@ -35,19 +34,25 @@ function myScope() {
         let n12 = Number((13.13 / 100 * n).toFixed(1))
 
         res.innerHTML += `<p>Para receber R$:<b>${n}</b> ficaria:</p>`
-        res.innerHTML += `<p><b>1X:</b> ${n} = R$:${n + n1}</p>` 
-        res.innerHTML += `<p><b>2X:</b> ${((n + n2) / 2).toFixed(2)} = R$:${n + n2}</p>`
-        res.innerHTML += `<p><b>3X:</b> ${((n + n3) / 3).toFixed(2)} = R$:${n + n3}</p>`
-        res.innerHTML += `<p><b>4X:</b> ${((n + n4) / 4).toFixed(2)} = R$:${n + n4}</p>`
-        res.innerHTML += `<p><b>5X:</b> ${((n + n5) / 5).toFixed(2)} = R$:${n + n5}</p>`
-        res.innerHTML += `<p><b>6X:</b> ${((n + n6) / 6).toFixed(2)} = R$:${n + n6}</p>`
-        res.innerHTML += `<p><b>7X:</b> ${((n + n7) / 7).toFixed(2)} = R$:${n + n7}</p>`
-        res.innerHTML += `<p><b>8X:</b> ${((n + n8) / 8).toFixed(2)} = R$:${n + n8}</p>`
-        res.innerHTML += `<p><b>9X:</b> ${((n + n9) / 9).toFixed(2)} = R$:${n + n9}</p>`
-        res.innerHTML += `<p><b>10X:</b> ${((n + n10) / 10).toFixed(2)} = R$:${n + n10}</p>`
-        res.innerHTML += `<p><b>11X:</b> ${((n + n11) / 11).toFixed(2)} = R$:${n + n11}</p>`
-        res.innerHTML += `<p><b>12X:</b> ${((n + n12) / 12).toFixed(2)} = R$:${n + n12}</p>`
+        res.innerHTML += `<p><b>1X:</b> ${n} = R$:${n + n1} ${createB(n1)}</p>`
+        res.innerHTML += `<p><b>2X:</b> ${((n + n2) / 2).toFixed(2)} = R$:${n + n2} ${createB(n2)}</p>`
+        res.innerHTML += `<p><b>3X:</b> ${((n + n3) / 3).toFixed(2)} = R$:${n + n3} ${createB(n3)}</p>`
+        res.innerHTML += `<p><b>4X:</b> ${((n + n4) / 4).toFixed(2)} = R$:${n + n4} ${createB(n4)}</p>`
+        res.innerHTML += `<p><b>5X:</b> ${((n + n5) / 5).toFixed(2)} = R$:${n + n5} ${createB(n5)}</p>`
+        res.innerHTML += `<p><b>6X:</b> ${((n + n6) / 6).toFixed(2)} = R$:${n + n6} ${createB(n6)}</p>`
+        res.innerHTML += `<p><b>7X:</b> ${((n + n7) / 7).toFixed(2)} = R$:${n + n7} ${createB(n7)}</p>`
+        res.innerHTML += `<p><b>8X:</b> ${((n + n8) / 8).toFixed(2)} = R$:${n + n8} ${createB(n8)}</p>`
+        res.innerHTML += `<p><b>9X:</b> ${((n + n9) / 9).toFixed(2)} = R$:${n + n9} ${createB(n9)}</p>`
+        res.innerHTML += `<p><b>10X:</b> ${((n + n10) / 10).toFixed(2)} = R$:${n + n10} ${createB(n10)}</p>`
+        res.innerHTML += `<p><b>11X:</b> ${((n + n11) / 11).toFixed(2)} = R$:${n + n11} ${createB(n11)}</p>`
+        res.innerHTML += `<p><b>12X:</b> ${((n + n12) / 12).toFixed(2)} = R$:${n + n12} ${createB(n12)}</p>`
     }
+
+    function createB(b) {
+        return `<b class="sale"> &uarr;${b}</b>`
+    }
+
+
 
 }
 
