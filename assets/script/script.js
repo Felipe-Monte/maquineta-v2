8f52bb09f8ffca0a52a656640ac06d46b4b0e2cf
felipe <carlosfelipemonte1997@hotmail.com>
2024-06-12 10:14:27 -0300

    refactoring

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,81 +1,38 @@
+const btn = document.getElementById("botao");
+const res = document.querySelector("#res");
+res.innerHTML = "";
 
-(function () {
-    const btn = document.getElementById('botao')
-    const res = document.querySelector('#res')
-    res.innerHTML = ''
+btn.addEventListener("click", function () {
+  let num = document.getElementById("txtn1");
+  let numUser = Number(num.value);
+  res.innerHTML = "";
+  num.focus();
 
-    btn.addEventListener('click', function () {
-        let num = document.getElementById('txtn1')
-        let numUser = Number(num.value)
-        res.innerHTML = ''
-        num.focus()
+  if (numUser > 50000 || numUser === 0 || numUser < 1) {
+    num.value = "";
+    num.focus();
+    return alert("Valor inválido");
+  }
+  renderRes(numUser);
+});
 
-        if (numUser > 50000 || numUser === 0 || numUser < 1) {
-            num.value = ''
-            num.focus()
-            return alert('Valor inválido')
-        }
-        renderRes(numUser)
-    })
+const Fees = [
+  6.38, 4.73, 5.51, 6.32, 7.12, 7.94, 8.98, 9.82, 10.64, 11.45, 12.29, 13.13,
+];
 
-    function renderRes(n) {
-        let n1 = Number((6.38 / 100 * n).toFixed(1))
-        let n2 = Number((4.73 / 100 * n).toFixed(1))
-        let n3 = Number((5.51 / 100 * n).toFixed(1))
-        let n4 = Number((6.32 / 100 * n).toFixed(1))
-        let n5 = Number((7.12 / 100 * n).toFixed(1))
-        let n6 = Number((7.94 / 100 * n).toFixed(1))
-        let n7 = Number((8.98 / 100 * n).toFixed(1))
-        let n8 = Number((9.82 / 100 * n).toFixed(1))
-        let n9 = Number((10.64 / 100 * n).toFixed(1))
-        let n10 = Number((11.45 / 100 * n).toFixed(1))
-        let n11 = Number((12.29 / 100 * n).toFixed(1))
-        let n12 = Number((13.13 / 100 * n).toFixed(1))
+function renderRes(n) {
+  res.innerHTML += `<p>PARA RECEBER R$:<b>${n}</b> FICARIA:</p>`;
 
-        res.innerHTML += `<p>PARA RECEBER R$:<b>${n}</b> FICARIA:</p>`
-        res.innerHTML += `<p><b>1X:</b> ${n} = R$:${n + n1} ${createB(n1)}</p>`
-        res.innerHTML += `<p><b>2X:</b> ${((n + n2) / 2).toFixed(2)} = R$:${n + n2} ${createB(n2)}</p>`
-        res.innerHTML += `<p><b>3X:</b> ${((n + n3) / 3).toFixed(2)} = R$:${n + n3} ${createB(n3)}</p>`
-        res.innerHTML += `<p><b>4X:</b> ${((n + n4) / 4).toFixed(2)} = R$:${n + n4} ${createB(n4)}</p>`
-        res.innerHTML += `<p><b>5X:</b> ${((n + n5) / 5).toFixed(2)} = R$:${n + n5} ${createB(n5)}</p>`
-        res.innerHTML += `<p><b>6X:</b> ${((n + n6) / 6).toFixed(2)} = R$:${n + n6} ${createB(n6)}</p>`
-        res.innerHTML += `<p><b>7X:</b> ${((n + n7) / 7).toFixed(2)} = R$:${n + n7} ${createB(n7)}</p>`
-        res.innerHTML += `<p><b>8X:</b> ${((n + n8) / 8).toFixed(2)} = R$:${n + n8} ${createB(n8)}</p>`
-        res.innerHTML += `<p><b>9X:</b> ${((n + n9) / 9).toFixed(2)} = R$:${n + n9} ${createB(n9)}</p>`
-        res.innerHTML += `<p><b>10X:</b> ${((n + n10) / 10).toFixed(2)} = R$:${n + n10} ${createB(n10)}</p>`
-        res.innerHTML += `<p><b>11X:</b> ${((n + n11) / 11).toFixed(2)} = R$:${n + n11} ${createB(n11)}</p>`
-        res.innerHTML += `<p><b>12X:</b> ${((n + n12) / 12).toFixed(2)} = R$:${n + n12} ${createB(n12)}</p>`
-    }
+  Fees.forEach((fee, index) => {
+    let calc = Number(((fee / 100) * n).toFixed(1));
+    let sum = (calc + n).toFixed(2);
 
-    function createB(b) {
-        return `<b class="sale"> &uarr;${b}</b>`
-    }
-})()
+    res.innerHTML += `<p><b>${index + 1}X:</b> ${(sum / (index + 1)).toFixed(
+      2
+    )} = R$:${sum} ${createB(calc)}</p>`;
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function createB(b) {
+    return `<b class="sale"> &uarr;${b}</b>`;
+  }
+}
--- assets/script/teste.js
+++ assets/script/teste.js
@@ -1,18 +1,46 @@
-function renderRes(n) {
-  let n2 = Number((4.73 / 100 * n).toFixed(1))
-  let n3 = Number((5.51 / 100 * n).toFixed(1))
-  let n4 = Number((6.32 / 100 * n).toFixed(1))
-  let n5 = Number((7.12 / 100 * n).toFixed(1))
-  console.log(n2 , n3, n4, n5)
+
+const Fees = [6.38, 4.73, 5.51, 6.32, 7.12, 7.94, 8.98, 9.82, 10.64, 11.45, 12.29, 13.13]
+
+function renderRes(n){
+  res.innerHTML += `<p>PARA RECEBER R$:<b>${n}</b> FICARIA:</p>`;
+
+  Fees.forEach((fee, index) => {
+    let calc = Number((fee / 100 * n).toFixed(1))
+    let sum = (calc + n).toFixed(2)
+    
+    res.innerHTML += `<p><b>${index + 1}X:</b> ${(sum / (index + 1)).toFixed(2)} = R$:${sum} ${createB(calc)}</p>`;
+  })
+  
+
+  function createB(b) {
+    return `<b class="sale"> &uarr;${b}</b>`;
+  }
 }
 renderRes(120)
 
-// const taxas = [6.38, 4.73, 5.51, 6.32, 7.12, 7.94, 8.98, 9.82, 10.64, 11.45, 12.29, 13.13]
 
-const taxas = [4.73]
 
-for(let i = 0; i < taxas.length; i++){
-  let result = taxas[i]
-  console.log("------")
-  console.log(result)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
